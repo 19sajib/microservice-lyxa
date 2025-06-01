19b737488056ec0a402660194377a0f0ab285e15
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2025-06-01 22:52:01 +0600

    feat(auth): db module rabbitmq module

---
+++ auth-service/src/rmq/rmq.module.ts
@@ -0,0 +1,35 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+interface RmqModuleOptions {
+  name: string; // The name of the client (e.g., AUTH_SERVICE, ORDER_SERVICE)
+}
+
+@Module({})
+export class RmqModule {
+  static register({ name }: RmqModuleOptions): DynamicModule {
+    return {
+      module: RmqModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name, // The name of the client to inject
+            useFactory: (configService: ConfigService) => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.get<string>('RABBITMQ_URI')],
+                queue: name, // Queue name for this client
+                queueOptions: {
+                  durable: false,
+                },
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
